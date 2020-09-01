a9bd70442cca8fbd0de8788de06a20f83272510b
Anthony Martin <anthony.ct.martin@gmail.com>
2020-09-01 08:24:44 -0700

    Experimenting with type generation

--- src/Bicep.Core/TypeSystem/TypeProperty.cs
+++ src/Bicep.Core/TypeSystem/TypeProperty.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+using System;
+
 namespace Bicep.Core.TypeSystem
 {
-    public class TypeProperty
+    public class TypeProperty : ITypeProperty
     {
         public TypeProperty(string name, TypeSymbol type, TypePropertyFlags flags = TypePropertyFlags.None)
         {
@@ -16,5 +18,32 @@ namespace Bicep.Core.TypeSystem
         public TypeSymbol Type { get; }
 
         public TypePropertyFlags Flags { get; }
+    }
+
+    public class LazyTypeProperty : ITypeProperty
+    {
+        private readonly Lazy<TypeSymbol> lazyType;
+
+        public LazyTypeProperty(string name, Func<TypeSymbol> typeFunc, TypePropertyFlags flags = TypePropertyFlags.None)
+        {
+            this.Name = name;
+            this.lazyType = new Lazy<TypeSymbol>(typeFunc, isThreadSafe: false);
+            this.Flags = flags;
+        }
+
+        public string Name { get; }
+
+        public TypeSymbol Type => lazyType.Value;
+
+        public TypePropertyFlags Flags { get; }
+    }
+
+    public interface ITypeProperty
+    {
+        string Name { get; }
+
+        TypeSymbol Type { get; }
+
+        TypePropertyFlags Flags { get; }
     }
 }
